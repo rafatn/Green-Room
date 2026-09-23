aacd627ff4c8b55ed6183e034f437f57eb5eb48a
rafatn <raftn4@gmail.com>
2026-09-23 09:25:30 +0300

    Change single quotes to double quotes in config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // Green Room V5.5 frontend configuration.
 // Leave empty while using static mode.
 // After deploying FastAPI, set the public HTTPS URL below.
-window.GREEN_ROOM_API_BASE = 'https://green-room-api.onrender.com';
+window.GREEN_ROOM_API_BASE = "https://green-room-api.onrender.com";
